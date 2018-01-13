f553a71a479e22a21eab77c65db53d3af6440397
MrazTevin <millatevin@gmail.com>
2018-01-13 20:18:02 +0300

    imported the repository

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient}  from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import {User} from './user'
+import {Repository} from './repository'
 @Injectable()
 export class GithubService {
 
