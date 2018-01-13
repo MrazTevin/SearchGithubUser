1e69d806b86c5d652b443c808c6feeb53c4a2cde
MrazTevin <millatevin@gmail.com>
2018-01-13 10:21:59 +0300

    made some chages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import {GithubService} from './github.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'app';
